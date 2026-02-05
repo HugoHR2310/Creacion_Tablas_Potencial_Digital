da810d247a7fbb06dfa2befad139b4477361896b
HugoHR2310 <hugohernandezroyano.guadalupe@alumnado.fundacionloyola.net>
2026-02-05 12:18:14 +0100

    Add files via upload

---
+++ Insert 2 id iguales (colegios).sql
@@ -0,0 +1,2 @@
+INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto)
+VALUES (2, 'Evg', 'Pepa', 123456789);
---
+++ Insert 2 id iguales (colegios_actividades).sql
@@ -0,0 +1,4 @@
+INSERT INTO colegios_actividades (idColegio, idCharla, fecha, numero_asistentes)
+VALUES 
+(2, 9, '2026-01-01', 13),
+(2, 10, '2026-05-03', 40);
